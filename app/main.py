e5751181db31aa3f7fabbd1513baa43556ac2cab
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-11-18 16:30:07 -0300

    Feat: implement flask app and requirements

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import Flask
+import request
+
+orders = [
+    {'id': 0, 'item': 'Test', 'quantity': 3} # Orders list
+]
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET']) # Health endpoint
+def health_check():
+    return 'OK', 200
+
+@app.route('/orders', methods=['GET', 'POST'])
+def orders_requests():
+    if request.method == 'POST': # Case POST add new order
+        new_order = request.get_json(orders) 
+        if new_order is None: # Validation
+            return 'Invalid order', 400
+        orders.append(new_order)
+        return f"{new_order} received", 201
+    else:
+        return {'orders': orders}, 200 # Case GET return all orders
+    
+
