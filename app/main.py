aad0ad231fc04392fb688c12fcbd05c1f3a1085f
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-11-24 21:17:02 -0300

    fix: requirements name, app POST method / add: gitignore file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-import Flask
-import request
+from flask import Flask, request, jsonify
+import os
 
 orders = [
     {'id': 0, 'item': 'Test', 'quantity': 3} # Orders list
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 @app.route('/health', methods=['GET']) # Health endpoint
 def health_check():
-    return 'OK', 200
+    return 'Server health', 200
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders_requests():
     if request.method == 'POST': # Case POST add new order
-        new_order = request.get_json(orders) 
+        new_order = request.get_json() 
         if new_order is None: # Validation
             return 'Invalid order', 400
         orders.append(new_order)
-        return f"{new_order} received", 201
+        return jsonify(new_order=new_order), 201
     else:
         return {'orders': orders}, 200 # Case GET return all orders
     
